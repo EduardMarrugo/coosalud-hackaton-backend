8266021be5e7727e0c0525f7de18337495972178
EDUAR CAMILO MARRUGO MARMOLEJO <eduarmarrugocm@unimagdalena.edu.co>
2023-09-14 11:57:43 -0500

    first commit

---
+++ Base/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Base.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public readonly IConfiguration configuration;
+        public AuthController(IConfiguration configuration) {
+            this.configuration = configuration;
+        }
+
+        public static User user = new User();
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<User>> Register(UserDTO request){
+
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+            user.Username = request.Username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            return Ok(user);
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+
+
+        }
+
+
+
+    }
+}
---
+++ Base/UserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Base
+{
+    public class UserDTO
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
